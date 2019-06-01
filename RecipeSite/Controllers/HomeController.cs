9b2d4ecc29c1fc93571e85bbb8bc4f4faa7343f7
DrDiabolical <egeh2x@gmail.com>
2019-06-01 19:51:02 -0400

    Add project files.

---
+++ RecipeSite/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+        public ViewResult RecipeList()
+        {
+            return View();
+        }
+        public ViewResult AddRecipe()
+        {
+            return View();
+        }
+        public ViewResult ViewRecipe()
+        {
+            return View();
+        }
+        public ViewResult ReviewRecipe()
+        {
+            return View();
+        }
+    }
+}
